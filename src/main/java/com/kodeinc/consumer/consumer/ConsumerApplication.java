a0dac28df760ff5265d7119b84d41ccb505bcc52
Muyinda ROgers <moverr@gmail.com>
2020-06-09 01:59:20 +0300

    :Debugggging

--- src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
+++ src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.messaging.SubscribableChannel;
@@ -14,24 +15,26 @@ import org.springframework.messaging.SubscribableChannel;
 @SpringBootApplication
 @EnableBinding(ConsumerChannels.class)
 public class ConsumerApplication {
-    private static final Logger LOG = Logger.getLogger(ConsumerApplication.class.getName());
-
+  
     
     @Bean
-    IntegrationFlow integrationFlow(ConsumerChannels c) {
+    IntegrationFlow integrationFlow(Logger logger, ConsumerChannels c) {
         return IntegrationFlows.from(c.producer())
                 .handle(String.class, (payload, header) -> {
-                    LOG.info("New Message "+ payload);
+                    logger.info("New Message "+ payload);
                     return null;
                 })
                 .get(); 
     }
     
+    @Bean
+    @Scope("prototype")       
     Logger logger(InjectionPoint ip){
         return Logger.getLogger(ip.getDeclaredType().getName());
     }
      
 
+    
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
@@ -40,6 +43,6 @@ public class ConsumerApplication {
 
 interface ConsumerChannels {
 
-    @Input("consumer")
+    @Input
     SubscribableChannel producer();
 }
