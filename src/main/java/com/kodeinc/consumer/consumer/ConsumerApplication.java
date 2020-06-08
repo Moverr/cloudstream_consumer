bf642ed2360f2b158a70755fd2bc2815aaae95f2
Muyinda ROgers <moverr@gmail.com>
2020-06-08 23:43:06 +0300

    IntegrationFlow

--- src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
+++ src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
@@ -4,20 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.messaging.SubscribableChannel;
 
 @SpringBootApplication
 @EnableBinding(ConsumerChannels.class)
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    IntegrationFlow integrationFlow(ConsumerChannels c) {
+        return IntegrationFlows.from(c.producer())
+                .handle(String.class, (payload, header) -> {
+                    return null;
+                })
+                .get();
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
 }
 
+interface ConsumerChannels {
 
-interface ConsumerChannels{
     @Input
     SubscribableChannel producer();
 }
