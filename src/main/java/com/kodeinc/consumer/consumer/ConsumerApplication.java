9a2fb1a5ce2a316394f6d32c98229ba33cdc57ec
Muyinda ROgers <moverr@gmail.com>
2020-06-09 00:19:29 +0300

    The Logger I know

--- src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
+++ src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
@@ -1,9 +1,11 @@
 package com.kodeinc.consumer.consumer;
 
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.messaging.SubscribableChannel;
@@ -11,7 +13,10 @@ import org.springframework.messaging.SubscribableChannel;
 @SpringBootApplication
 @EnableBinding(ConsumerChannels.class)
 public class ConsumerApplication {
+    private static final Logger LOG = Logger.getLogger(ConsumerApplication.class.getName());
 
+    
+    @Bean
     IntegrationFlow integrationFlow(ConsumerChannels c) {
         return IntegrationFlows.from(c.producer())
                 .handle(String.class, (payload, header) -> {
@@ -20,6 +25,8 @@ public class ConsumerApplication {
                 .get();
 
     }
+    
+    
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
