28554e9f371eb5f3b31e45372ee73ecfa741f9b7
Muyinda ROgers <moverr@gmail.com>
2020-06-09 03:32:38 +0300

    Debugging

--- src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
+++ src/main/java/com/kodeinc/consumer/consumer/ConsumerApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.SubscribableChannel;
 
 @SpringBootApplication
@@ -17,6 +19,12 @@ import org.springframework.messaging.SubscribableChannel;
 public class ConsumerApplication {
   
     
+        @StreamListener()
+	public void handleMessage(String message){
+		System.out.println("Subscriber Received Message is: " + message);
+	}
+        
+        
     @Bean
     IntegrationFlow integrationFlow(Logger logger, ConsumerChannels c) {
         return IntegrationFlows.from(c.producer())
